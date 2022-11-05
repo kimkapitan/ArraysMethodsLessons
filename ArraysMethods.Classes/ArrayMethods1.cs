09d6e5bb930acc6e71de920b228ff180ad7f13ca
Aleksander Kolmogorov <aleksander.kolmogorov@kandasoft.com>
2022-11-05 20:47:38 +0600

    added ArrayMethods1

---
+++ ArraysMethods.Classes/ArrayMethods1.cs
@@ -0,0 +1,35 @@
+﻿namespace ArraysMethods.Classes
+{
+    public static class ArrayMethods1
+    {
+        /// <summary>
+        /// Return Sum of all elements of array
+        /// </summary>
+        /// <param name="arr"> Array of numbers </param>
+        /// <returns> Sum of elements </returns>
+        public static int GetSum(int[] arr)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Return Average value of all elements of array
+        /// </summary>
+        /// <param name="arr"> Array of numbers </param>
+        /// <returns> Average value of elements (Round to 2 digits) </returns>
+        public static double GetAvg(int[] arr)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Return Median value of all elements of array
+        /// </summary>
+        /// <param name="arr"> Array of numbers </param>
+        /// <returns> Median value of elements (Round to 2 digits) </returns>
+        public static double GetMedian(int[] arr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ArraysMethods.Tests/ArrayMethodsTests1.cs
@@ -0,0 +1,73 @@
+﻿using ArraysMethods.Classes;
+
+namespace ArraysMethods.Tests
+{
+    public class ArrayMethodsTests1
+    {
+        [Fact]
+        public void GetSum_ShouldReturnValidValue_ExampleArray()
+        {
+            //Arrange
+            var arr = new int[] { 1, 2, 3 };
+            //Act
+            var result = ArrayMethods1.GetSum(arr);
+            //Assert
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void GetSum_ShouldReturnValidValue_RandomArray()
+        {
+            //Arrange
+            var arr = new int[] { 15, 8, 6, 9, 13, 7, 14, 5, 100, 17, 12, 2, 4, 10, 11, 20, 1, 16, 18, 1, 20, 0 };
+            //Act
+            var result = ArrayMethods1.GetSum(arr);
+            //Assert
+            Assert.Equal(309, result);
+        }
+
+        [Fact]
+        public void GetAvg_ShouldReturnValidValue_ExampleArray()
+        {
+            //Arrange
+            var arr = new int[] { 1, 2, 3 };
+            //Act
+            var result = ArrayMethods1.GetAvg(arr);
+            //Assert
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void GetAvg_ShouldReturnValidValue_RandomArray()
+        {
+            //Arrange
+            var arr = new int[] { 15, 8, 6, 9, 13, 7, 14, 5, 100, 17, 12, 2, 4, 10, 11, 20, 1, 16, 18, 1, 20, 0 };
+            //Act
+            var result = ArrayMethods1.GetAvg(arr);
+            //Assert
+            Assert.Equal(14.05, result);
+        }
+
+        [Fact]
+        public void GetMedian_ShouldReturnValidValue_ExampleArray()
+        {
+            //Arrange
+            var arr = new int[] { 1, 2, 3 };
+            //Act
+            var result = ArrayMethods1.GetMedian(arr);
+            //Assert
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void GetMedian_ShouldReturnValidValue_RandomArray()
+        {
+            //Arrange
+            var arr = new int[] { 15, 8, 6, 9, 13, 7, 14, 5, 100, 17, 12, 2, 4, 10, 11, 20, 1, 16, 18, 1, 20, 0 };
+            //Act
+            var result = ArrayMethods1.GetMedian(arr);
+            //Assert
+            Assert.Equal(10.5, result);
+        }
+    }
+}
